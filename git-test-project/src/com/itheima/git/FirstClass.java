cd8d9cacc5a8046e6117a4881d1f6397f3583aba
liufang <1710845940@qq.com>
2020-06-21 18:22:07 +0800

    提交git-test-project的FirstClass

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,28 @@
+package com.itheima.git;
+/**
+ * @Author LiuFang
+ */
+public class FirstClass {
+    /**
+     * @Param
+     * @Return 
+     */
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+
+    public void test(){
+        int i = 0;
+        i += 10;
+    }
+
+}
